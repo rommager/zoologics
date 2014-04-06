0b3a18174946a5648c2cf3241b201bdcafd30b56
casweet8 <chasesweet@gmail.com>
2014-04-05 20:02:05 -0400

    Chase's additions for the outline of the program

--- ZooLogics/src/edu/radford/itec370/mainmethod/zoologic/io/disk/DataIO.java
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologic/io/disk/DataIO.java
@@ -1,5 +1,42 @@
 package edu.radford.itec370.mainmethod.zoologic.io.disk;
 
+import java.util.ArrayList;
+
+import edu.radford.itec370.mainmethod.zoologic.Animal;
+import edu.radford.itec370.mainmethod.zoologic.Species;
+import edu.radford.itec370.mainmethod.zoologic.Task;
+import edu.radford.itec370.mainmethod.zoologic.Vaccine;
+
 public class DataIO {
+
+	public void saveAnimal (ArrayList <Animal> animal){
+		
+	}
+	
+	public void saveTask (ArrayList <Task> task){
+		
+	}
+	
+	public ArrayList <Animal> loadAnimals() {
+		return null;
+	
+	}
+	
+	public ArrayList <Task> loadTask(){
+	
+		return null;
+	}
+	
+	public void loadReferenceData(ArrayList <Vaccine> vaccine, ArrayList<Species> species){
+		
+	}
+	
+	public void createBackup() {
+	
+	}
+		
+	
+
+	
 
 }
