175536155994174f597ca0453ec571ce8120010d
Zabbitt92 <zabbitt@radford.edu>
2014-04-05 20:13:04 -0400

    Initial variables and stub methods.

---
+++ Zoologic/src/edu/radford/itec370/mainmethod/zoologic/Vaccine.java
@@ -0,0 +1,10 @@
+package edu.radford.itec370.mainmethod.zoologic;
+
+public class Vaccine {
+	private int vaccineID;
+	private String vaccineName;
+	private String dosage;
+	private boolean recurring;
+	private int frequencyDays;
+	private int dueAtAnimalAge;
+}
