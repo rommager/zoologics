0f187f0edeacd49121edaa6063e3215af9e5d15c
Patrick Draper <pdogg1024@hotmail.com>
2014-04-05 20:38:54 -0400

    Patrick committing and installing Staff

--- ZooLogics/src/edu/radford/itec370/mainmethod/zoologic/Staff.java
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologic/Staff.java
@@ -2,4 +2,15 @@ package edu.radford.itec370.mainmethod.zoologic;
 
 public class Staff {
 
+    private String name;
+    private boolean isUser;
+    private String passwordHash;
+
+public void validate(String username, String Password) {
+
+    
 }
+
+}
+
+
