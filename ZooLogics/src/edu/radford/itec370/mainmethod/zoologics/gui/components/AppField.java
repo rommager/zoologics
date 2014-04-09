238807dfbd8a39fa4605475b671ea0ca4f1ba0f8
Sean <Sean@byrdzu>
2014-04-09 09:23:07 -0400

    Fixed error on work in progress

---
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/gui/components/AppField.java
@@ -0,0 +1,15 @@
+package edu.radford.itec370.mainmethod.zoologics.gui.components;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class AppField extends JPanel 
+{
+	private JLabel label;
+	private AppTextBox textBox;
+	
+	public AppField() 
+	{
+		
+	}
+}
