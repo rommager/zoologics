09eb67b1e48f98f4e52359c53959996c13b13f31
rommager <rommager@cox.net>
2014-04-10 07:45:45 -0400

    Delete Species.java

--- Zoologic/src/edu/radford/itec370/mainmethod/zoologic/Species.java
+++
@@ -1,8 +0,0 @@
-package edu.radford.itec370.mainmethod.zoologic;
-
-import java.util.ArrayList;
-
-public class Species {
-	private String speciesName;
-	private ArrayList<Vaccine> vaccineIdCollection = new ArrayList<Vaccine>();
-}
