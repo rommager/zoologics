435530cf6a8c2415388c8034c2fba46993dff0ee
Sean <Sean@byrdzu>
2014-04-10 19:11:59 -0400

    test commit

--- ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/gui/AnimalPanel.java
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/gui/AnimalPanel.java
@@ -5,9 +5,47 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 import edu.radford.itec370.mainmethod.zoologics.Animal;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AnimalPanel extends JPanel {
+	public AnimalPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 3;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.anchor = GridBagConstraints.WEST;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.anchor = GridBagConstraints.BASELINE;
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		add(btnNewButton, gbc_btnNewButton);
+	}
 	private ArrayList <Animal> animals;
+	private JTextField textField;
 	
 	public void refresh() {
 		
---
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/gui/LogonDialog.java
@@ -0,0 +1,94 @@
+package edu.radford.itec370.mainmethod.zoologics.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.border.EmptyBorder;
+
+import edu.radford.itec370.mainmethod.zoologics.*;
+import edu.radford.itec370.mainmethod.zoologics.gui.components.*;
+import javax.swing.JPasswordField;
+
+public class LogonDialog extends JDialog {
+	protected final static String WINDOW_TITLE = Application.getAppName() + " Logon";
+	
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtUserName;
+	private JLabel lblUsername;
+	private JLabel lblPassword;
+	private JPasswordField passwordField;
+	//private Staff staff;
+	
+	public LogonDialog() 
+	{
+		super();
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setTitle(WINDOW_TITLE);
+		setIconImage(Application.getAppIcon());
+		setResizable(false);
+		setModalityType(ModalityType.APPLICATION_MODAL);
+		setBounds(100, 100, 246, 168);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			lblUsername = new JLabel("User name: ");
+			lblUsername.setBounds(10, 24, 84, 14);
+			contentPanel.add(lblUsername);
+		}
+		{
+			txtUserName = new JTextField();
+			txtUserName.setBounds(115, 21, 100, 20);
+			contentPanel.add(txtUserName);
+			txtUserName.setColumns(10);
+		}
+		{
+			lblPassword = new JLabel("Password:");
+			lblPassword.setBounds(10, 65, 84, 14);
+			contentPanel.add(lblPassword);
+		}
+		{
+			passwordField = new JPasswordField();
+			passwordField.setBounds(115, 62, 100, 20);
+			contentPanel.add(passwordField);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						System.exit(0);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		setVisible(true);
+	}
+}
