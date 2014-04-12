0c08fdc0be315ba010db1edbcac31f82acd5176c
Zabbitt92 <zabbitt@radford.edu>
2014-04-12 18:11:47 -0400

    Species Panel with unfinished table.

---
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/gui/SpeciesPanel.java
@@ -0,0 +1,77 @@
+package edu.radford.itec370.mainmethod.zoologics.gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+
+public class SpeciesPanel extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			SpeciesPanel dialog = new SpeciesPanel();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public SpeciesPanel() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SpeciesPanel.class.getResource("/edu/radford/itec370/mainmethod/zoologics/z_icon.png")));
+		setTitle("Species");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblSpecies = new JLabel("Species:");
+			lblSpecies.setBounds(10, 11, 46, 14);
+			contentPanel.add(lblSpecies);
+		}
+		
+		textField = new JTextField();
+		textField.setBounds(66, 8, 133, 20);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		{
+			table = new JTable();
+			table.setBounds(20, 36, 393, 182);
+			contentPanel.add(table);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
