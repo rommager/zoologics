3ec9812b9538efbaf8576861c79e6b2b1a745dc9
Sean <Sean@byrdzu>
2014-04-18 00:00:31 -0400

    Messed around with all task stuff, and got complex recurrence working

--- ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/Task.java
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/Task.java
@@ -1,62 +1,152 @@
 package edu.radford.itec370.mainmethod.zoologics;
 
+import java.io.Serializable;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Queue;
 
-public abstract class Task {
+public class Task implements Serializable {
 
+	public static final int ACTIVE = 1;
+	public static final int COMPLETED = 2;
+	public static final int DISMISSED = 3;
+	public static final int DELETED = 0;
+	
+	public static final int DAY = Calendar.DATE;
+	public static final int WEEK = Calendar.WEEK_OF_YEAR;
+	public static final int MONTH = Calendar.MONTH;
+	public static final int YEAR = Calendar.YEAR;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8687090435553311509L;
 	private StringBuffer description;
 	private Calendar dueDate;
-	private boolean recurring;
-	private TaskStatus status;
+	private int status;
+	
+	private TaskRecurrences recurrences;
+	
 	private TaskList parentTaskList;
-	private Queue<TaskRecurrence> recurrences;
 	
 	public Task() {
 		super();
 	}
 	
-	public Task(String description, TaskStatus status, 
+	public Task(StringBuffer description,
+			int status,
 			TaskList parentTaskList) {
 		this();
 		this.description = new StringBuffer(description);
 		this.status = status;
 		this.parentTaskList = parentTaskList;
-	
 	}
 	
-	public Task(String description, TaskStatus status, 
-			TaskList parentTaskList, boolean recurring) {
+	public Task(StringBuffer description, 
+			int status,
+			TaskList parentTaskList,
+			Calendar dueDate) {
 		this(description, status, parentTaskList);
-		this.recurring = recurring;
-	}
-	
-		
-	public Task(String description, TaskStatus status, 
-			TaskList parentTaskList, boolean recurring, Calendar dueDate) {
-		this(description, status, parentTaskList, recurring);
 		this.dueDate = dueDate;
 	}
-
-	public Task(String description, TaskStatus status, 
-			TaskList parentTaskList, boolean recurring,
+	
+	// Constructor that accepts strings for description and due date, for IO
+	public Task(String description, 
+			int status, 
+			TaskList parentTaskList,
 			String dueDate) throws ParseException {
-		this(description, status, parentTaskList, recurring);
+		this(new StringBuffer(description), status, parentTaskList);
 		Calendar dueCalendar = Calendar.getInstance();
 		dueCalendar.setTime(Application.getDateFormat().parse(dueDate));
 		setDueDate(dueCalendar);
 	}
 
+	public static void main(String[] args) {
+		TaskList list = new TaskList();
+		try {
+			Task newTask = new Task("Reminder to clean toilets", Task.ACTIVE, list, "11/17/2011");
+			TaskRecurrences newRecurrences = new TaskRecurrences();
+			newTask.setRecurrences(newRecurrences);
+			newRecurrences.add(new TaskRecurrenceInstance(3,WEEK,2));
+			newRecurrences.add(new TaskRecurrenceInstance(2,MONTH,1));
+			newRecurrences.add(new TaskRecurrenceInstance(-1,YEAR,1));
+			list.add(newTask);
+			list.add(new Task("Vet Visit for Puja", Task.ACTIVE, list, "05/17/2014"));
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+			newTask =  newTask.spawnNextTaskRecurrence();
+			System.out.println("SPAWNED: " + newTask);
+
+			System.out.println(list.toString());
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public void dismiss(){
-		setStatus(TaskStatus.DISMISSED);
+		setStatus(DISMISSED);
 	}
 	
 	public void complete() throws InvalidTaskCompletionException{
 		if (isValid()) {
-			setStatus(TaskStatus.COMPLETED);
-//			generateNextRecurrence();
+			setStatus(COMPLETED);
+			spawnNextTaskRecurrence();
 		}
 		else {
 			throw new InvalidTaskCompletionException();
@@ -67,14 +157,31 @@ public abstract class Task {
 		return true;
 	}
 	
-/*	private void generateNextRecurrence(){
-		if (recurring) {
-			Calendar newDate = (Calendar) dueDate.clone();
-			newDate.dueDate
+	public Task spawnNextTaskRecurrence(){
+		if (isRecurring()) {
+			Task newTask = new Task(description, status, parentTaskList);
+			
+			// move recurrences collection from this instance to the newly spawned instance
+			newTask.setRecurrences(this.getRecurrences());
+			newTask.setDueDate(recurrences.getNextRecurrenceDate(dueDate));
+			this.recurrences = null;
+			parentTaskList.add(newTask);
+			return newTask;
 		}
+		this.setRecurrences(null);
+		return null;
 	}
-*/
 	
+	public boolean isRecurring() {
+		return (recurrences != null);
+	}
+	
+	@Override
+	public String toString() {
+		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		return "Task '" + description + "', " + " status=" + status + ", due=" + df.format(dueDate.getTime()) + "]";
+	}
+
 	public StringBuffer getDescription() {
 		return description;
 	}
@@ -87,19 +194,11 @@ public abstract class Task {
 	public void setDueDate(Calendar dueDate) {
 		this.dueDate = dueDate;
 	}
-	public TaskStatus getStatus() {
+	public int getStatus() {
 		return status;
 	}
-	public void setStatus(TaskStatus status) {
+	public void setStatus(int status) {
 		this.status = status;
-	}
-
-	public boolean isRecurring() {
-		return recurring;
-	}
-
-	public void setRecurring(boolean recurring) {
-		this.recurring = recurring;
 	}
 
 	public TaskList getParentTaskList() {
@@ -110,12 +209,11 @@ public abstract class Task {
 		this.parentTaskList = parentTaskList;
 	}
 
-	public Queue<TaskRecurrence> getRecurrences() {
+	public TaskRecurrences getRecurrences() {
 		return recurrences;
 	}
 
-	public void setRecurrences(Queue<TaskRecurrence> recurrences) {
+	public void setRecurrences(TaskRecurrences recurrences) {
 		this.recurrences = recurrences;
 	}
-			
 }
---
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/TaskRecurrences.java
@@ -0,0 +1,30 @@
+package edu.radford.itec370.mainmethod.zoologics;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class TaskRecurrences extends ArrayList<TaskRecurrenceInstance> {
+
+	public TaskRecurrences() {
+		super();		
+	}
+	
+	public Calendar getNextRecurrenceDate(Calendar dueDate) {
+		while (this.size() != 0) {
+			TaskRecurrenceInstance ti = this.get(0);
+			
+			if (ti.hasMoreRecurrences()) {
+				Calendar newDate = (Calendar) dueDate.clone();
+				newDate.add(ti.getIntervalType(), ti.getIntervalAmount());
+				return newDate;
+			}
+			else {
+				// if no recurrences are left, remove this item from the arraylist as it is now expired.
+				this.remove(ti);				
+			}
+		}
+		 return null;	
+	}
+
+
+}
