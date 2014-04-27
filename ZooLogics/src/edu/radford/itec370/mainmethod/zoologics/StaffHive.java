3d4e517c52702c5aacf141e8f6217b331405b9c3
Sean <Sean@byrdzu>
2014-04-27 13:57:40 -0400

    Done with StaffHive now

--- ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/StaffHive.java
+++ ZooLogics/src/edu/radford/itec370/mainmethod/zoologics/StaffHive.java
@@ -5,16 +5,13 @@ import java.util.ArrayList;
 
 public class StaffHive extends ArrayList<Staff> implements Serializable {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5713056110616181317L;
 
 	public Staff findUser(String username) {
 		username.trim();
-		for (Staff staffer : this) {
-			if (username.equalsIgnoreCase(staffer.getUserName())) {
-				return staffer;
+		for (Staff staff : this) {
+			if (username.equalsIgnoreCase(staff.getUserName())) {
+				return staff;
 			}
 		}
 		return null;
