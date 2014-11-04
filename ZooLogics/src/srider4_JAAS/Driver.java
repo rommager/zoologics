407a7aec5a5b82a991ecae60657aaca26792e081
Sean <Sean@byrdzu>
2014-11-04 00:08:16 -0500

    commit

--- ZooLogics/src/srider4_JAAS/Driver.java
+++ ZooLogics/src/srider4_JAAS/Driver.java
@@ -9,6 +9,7 @@ public class Driver {
 	private static String[] positions =  {"CEO","VP","Manager","Associate","Junior Associate"};
 	private ArrayList<Employee> employees;
 	private DataIO<Employee> io;
+	private Scanner scan;
 	
 	public static void main(String[] args) {
 	
@@ -18,38 +19,67 @@ public class Driver {
 	
 	private void run() {
 		String filename = "src/srider4_JAAS/employees.txt";
-		employees = new ArrayList<Employee>();
-		
+		employees = new ArrayList<Employee>();		
 		io = new DataIO<Employee>(filename);		
 		employees = io.loadData(new Employee());
+		scan = new Scanner(System.in);
 		
-		for (Employee emp : employees) {
+		
+/*		for (Employee emp : employees) {
 			System.out.println(emp.toString());
-		}
+		}*/
 		
-		System.out.println("Login ID: ");
-		Scanner scanner = new Scanner(System.in);
-		int id = scanner.nextInt();
 		
-		user = getEmployee(id);
+		do {
+			System.out.println("Login ID: ");
+			
+			int id = scan.nextInt();		
+			user = getEmployee(id); 
+			if (user == null)
+				System.out.println("Invalid ID!"); }
+		while (user == null);
+		
+		
 		
 		System.out.println(user.toString());
 		
-		scanner.close();
+		int selection = getMenuSelection();
+		
+		scan.close();
 		
 	}
 	
-	private void showMenu() {
+	private void editEmployee(Employee employee) {
 		
 	}
 	
 	private Employee getEmployee(int id) {
+		
 		for (Employee emp : employees) {
 			if (id == emp.getId())
 				return emp;
 		}
 		return null;
 	}
+	
+	private void listEmployees() {
+		
+	}
+	
+	private int getMenuSelection() {
+		System.out.println("Select Option From Menu:");
+		System.out.println(" 1) View my information");
+		System.out.println(" 2) Edit an employee's information\n");
+		System.out.println(" 3) Log out");
+		System.out.println("Enter Option:");
+//		try {
+			int selection = scan.nextInt();
+//		}
+//		catch Exception e { }
+		return selection;
+	}
+	
+	
 	
 	
 
